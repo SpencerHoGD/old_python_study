758a998528b82ad2501ae5531ba7aaa85159ca78
hxm <125423559@qq.com>
2024-11-02 05:53:39 +0800

    test 005

--- 005touchfiles.py
+++ 005touchfiles.py
@@ -1,10 +1,10 @@
 import pathlib
 
-PATH = "/home/liam/test/test/"
+PATH = "/home/hxm/test"
 # PATH = 'images'
 
 pathlib.Path(PATH).mkdir(exist_ok=True)
 
-for fn in range(10**1):
-    with open(f"{PATH}/{fn}.txt", "w") as fp:
+for fn in range(10**3):
+    with open(f"{PATH}/{fn}.txt", "w", encoding="utf-8") as fp:
         fp.write(f"hello, pathlib {fn}")
