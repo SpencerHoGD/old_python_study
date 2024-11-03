42efb83393f5af52d594d0cd625cd1764c42583e
hxm <125423559@qq.com>
2024-11-03 18:14:01 +0800

    006

--- 006_test.py
+++ 006_test.py
@@ -1,45 +1,46 @@
+import pathlib
 from datetime import datetime
 
 import pandas as pd
 
-FILENAME1 = "/home/hxm/test/2024_Q1th.md"
-FILENAME2 = "/home/hxm/test/2024_Q2nd.md"
-FILENAME3 = "/home/hxm/test/2024_Q3rd.md"
+PATH = "/home/hxm/test/mds"
+
+pathlib.Path(PATH).mkdir(exist_ok=True)
+
+
 FILENAME4 = "/home/hxm/test/2024_Q4th.md"
-dates = pd.date_range(start="20240901", end="20241231", freq="D")
+
+monthes = pd.date_range(start="20240101", end="20241231", freq="M")
+dates = pd.date_range(start="20240101", end="20241231", freq="D")
 now = datetime.now()
 
+# print('## ' + now.strftime('%a %b %d %H:%m'))
 # print(
-#     "["
-#     + now.strftime("%d")
-#     + "]"
-#     + "("
+#     "## "
 #     + now.strftime("%Y/%m/%d %a")
 #     + " day="
 #     + now.strftime("%j")
 #     + " week="
 #     + now.strftime("%W")
-#     + ")"
 # )
 
-# print('## ' + now.strftime('%a %b %d %H:%m'))
-print(
-    "## "
-    + now.strftime("%Y/%m/%d %a")
-    + " day="
-    + now.strftime("%j")
-    + " week="
-    + now.strftime("%W")
-)
+for month in monthes:
+    print(month)
 
-with open(FILENAME4, "w", encoding="utf-8") as f:
-    for date in dates:
-        f.write(
-            "## "
-            + date.strftime("%Y/%m/%d %a")
-            + ". day"
-            + date.strftime("%j")
-            + " week"
-            + date.strftime("%W")
-            + "\n"
-        )
+#     with open(f"{PATH}/{month}.md", "w", encoding="utf-8") as fp:
+#         fp.write(f"hello, pathlib {month}")
+#
+# with open(FILENAME4, "w", encoding="utf-8") as f:
+#     for date in dates:
+#         f.write(
+#             "## "
+#             + date.strftime("%Y/%m/%d %a")
+#             + ". day"
+#             + date.strftime("%j")
+#             + " week"
+#             + date.strftime("%W")
+#             + "\n"
+#             + "\n"
+#             + "\n"
+#             + "\n"
+#         )
