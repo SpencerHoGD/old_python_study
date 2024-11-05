38f1c02d59dfcf395f7c83529443c13e936c33bc
hxm <125423559@qq.com>
2024-11-06 00:35:04 +0800

    changes

--- t001_dir.py
+++ t001_dir.py
@@ -11,6 +11,15 @@ def d():
     print(x)
 
 
+def f():
+    # Use "e" to convert a number into scientific number format (with a lower-case e):
+
+    txt = f"We have {5000000000:e} chickens."
+
+    print(txt)
+
+
 if __name__ == "__main__":
-    p()
-    d()
+    # p()
+    # d()
+    f()
